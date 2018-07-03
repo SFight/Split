e7773ec64ff31ed60b8903a684953ec51b65e785
vtears <772326244@qq.com>
2018-07-03 10:14:51 +0800

    Initial Commit

---
+++ VTSplit/VTSplit.h
@@ -0,0 +1,19 @@
+//
+//  VTSplit.h
+//  VTSplit
+//
+//  Created by vtears on 2018/7/3.
+//  Copyright © 2018年 vtears.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VTSplit.
+FOUNDATION_EXPORT double VTSplitVersionNumber;
+
+//! Project version string for VTSplit.
+FOUNDATION_EXPORT const unsigned char VTSplitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VTSplit/PublicHeader.h>
+
+
