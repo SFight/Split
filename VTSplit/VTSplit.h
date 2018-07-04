b20a93691781f075952531cc2e8e94a2b86af998
vtears <772326244@qq.com>
2018-07-04 16:03:03 +0800

    对外暴露Base和Split

--- VTSplit/VTSplit.h
+++ VTSplit/VTSplit.h
@@ -16,4 +16,7 @@ FOUNDATION_EXPORT const unsigned char VTSplitVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <VTSplit/PublicHeader.h>
 
+#import <VTSplit/VTBaseViewController.h>
+#import <VTSplit/VTSplitViewController.h>
+
 
